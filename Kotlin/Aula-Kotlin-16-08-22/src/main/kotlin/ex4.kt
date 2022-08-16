383c023fed24a97fb37a065074de02cc2c530c4d
Bruno Carlini <bruno-carlini@hotmail.com>
2022-08-16 15:17:27 -0300

    Exercício 4 realializado com sucesso

---
+++ Kotlin/Aula-Kotlin-16-08-22/src/main/kotlin/ex4.kt
@@ -0,0 +1,23 @@
+import kotlin.math.sqrt
+import kotlin.math.pow
+
+fun main() {
+    /*
+    * Faça um programa em que permita a entrada de um número qualquer e exiba se este
+      número é par ou ímpar. Se for par exiba também a raiz quadrada do mesmo; se for
+      ímpar exiba o número elevado ao quadrado.
+    * */
+
+
+    print("Informe um número: ")
+    val num = readln().toInt()
+
+    if(num % 2 == 0){
+        val raizQuad = sqrt(num.toFloat())
+        println("Número $num é par e a raiz quadrada dele é $raizQuad")
+    }else{
+        val elevadoQuad = num.toFloat().pow(2)
+        println("Número $num é par e a raiz quadrada dele é $elevadoQuad")
+
+    }
+}
