abe678a0c9427a841f962a08fbf7a0ccbf9769ea
Bruno Carlini <bruno-carlini@hotmail.com>
2022-08-17 16:20:20 -0300

    Update

--- Kotlin/Aula-Kotlin-16-08-22/src/main/kotlin/ex4.kt
+++ Kotlin/Aula-Kotlin-16-08-22/src/main/kotlin/ex4.kt
@@ -3,7 +3,7 @@ import kotlin.math.pow
 
 fun main() {
     /*
-    * Faça um programa em que permita a entrada de um número qualquer e exiba se este
+    *4- Faça um programa em que permita a entrada de um número qualquer e exiba se este
       número é par ou ímpar. Se for par exiba também a raiz quadrada do mesmo; se for
       ímpar exiba o número elevado ao quadrado.
     * */
