1519f59f48f2fd2543d213d5105874f4f604de64
Bruno Carlini <bruno-carlini@hotmail.com>
2022-08-17 16:18:37 -0300

    Update

--- Kotlin/Aula-Kotlin-17-08-22/src/main/kotlin/ex6-repeticaoDoWhile.kt
+++ Kotlin/Aula-Kotlin-17-08-22/src/main/kotlin/ex6-repeticaoDoWhile.kt
@@ -1,5 +1,5 @@
 fun main() {
-    //Escrever um programa que receba vários números inteiros no teclado. E no
+    //6- Escrever um programa que receba vários números inteiros no teclado. E no
     //final imprimir a média dos números múltiplos de 3. Para sair digitar
     //0(zero).(DO...WHILE)
 
