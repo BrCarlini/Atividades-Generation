55e4caa33c6fa6225e2a0269593c03583a31a060
Bruno Carlini <bruno-carlini@hotmail.com>
2022-08-19 15:45:29 -0300

    Adicionado comentario referenciando o arquivo das funções e adicionado o else do when

--- Kotlin/Aula-Kotlin-19-08-22/src/main/kotlin/ex1.kt
+++ Kotlin/Aula-Kotlin-19-08-22/src/main/kotlin/ex1.kt
@@ -11,6 +11,9 @@ fun main() {
     Apresentar todos os dados da list
     * */
 
+    
+    // As funções este progrma estão no arquivo funEx1.kt
+
     while(true){
         println("Menu:\n1 - Adicionar Item\n2 - Remover Item\n3 - Atualizar Item\n4 - Mostrar Itens Adicionados\n5 - Sair")
         print(">>> ")
@@ -22,6 +25,7 @@ fun main() {
             3 -> atualizar()
             4 -> exibirItem()
             5 -> break
+            else -> println("Valor Inválido")
         }
     }
 
