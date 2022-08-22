7be6d40a60e8d23c413df270d0f92c446c388d25
Bruno Carlini <bruno-carlini@hotmail.com>
2022-08-22 18:34:40 -0300

    Exercicio 4 realizado com sucesso - 4/7

---
+++ Kotlin/Ex-Kotlin-POO-22-08-22/src/main/kotlin/MainFuncionario.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val funcionario1 = Funcionario(
+        "Bruno",
+        "555.555.555.55",
+        "55555555-5",
+        "Desenvolvedor Mobile Jr",
+        "191834",
+        3500.0,
+    )
+    funcionario1.dataNasc = "11/11/1111"
+    funcionario1.endereco = "Av. Bosque Encantado"
+
+    print("Bateu a meta ? [1-Sim/2-Sim]: ")
+    val pergMeta = readln()
+
+    if(pergMeta == "1"){
+        print("Informe quantos porcento dara para o funcionario: ")
+        val porc = readln().toDouble()
+        println("Salário + Bonus de Meta Batida: ${funcionario1.bonusPorMeta(porc)}")
+    }else{
+        println("Se dedique mais!!")
+    }
+    println("=====================================================")
+    funcionario1.visualizarDados()
+
+
+}
