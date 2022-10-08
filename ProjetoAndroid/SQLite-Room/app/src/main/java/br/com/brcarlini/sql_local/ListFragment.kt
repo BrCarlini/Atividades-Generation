bc35ddf77943f038b625bc9d0c8d69a49c22ddfe
Bruno Carlini <bruno-carlini@hotmail.com>
2022-10-08 15:37:24 -0300

    Ex BD local finalizado

--- ProjetoAndroid/SQLite-Room/app/src/main/java/br/com/brcarlini/sql_local/ListFragment.kt
+++ ProjetoAndroid/SQLite-Room/app/src/main/java/br/com/brcarlini/sql_local/ListFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import br.com.brcarlini.sql_local.adapter.UserAdapter
 import br.com.brcarlini.sql_local.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
     private lateinit var binding: FragmentListBinding
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +22,17 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentListBinding.inflate(layoutInflater, container, false)
+
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        val adapter = UserAdapter()
+        binding.recyclerUser.layoutManager = LinearLayoutManager(context)
+        binding.recyclerUser.adapter = adapter
+        binding.recyclerUser.setHasFixedSize(true)
+
+        mainViewModel.selectUsers.observe(viewLifecycleOwner){
+            response -> adapter.setList(response)
+        }
 
         binding.floatingActionButton.setOnClickListener{
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
